02e814ea93fea94f35d971bbfe17e7ff50b9b84a
Mohammad <mohammad.amla.pro@gmail.com>
2019-11-28 13:17:40 +0100

    esaai
    essai php

--- Inscription.php
+++ Inscription.php
@@ -1,7 +1,16 @@
-<?php
-
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Liste des utilisateurs</title>
+	</head>
+	<body>
 /* ici tu vas caler tout ton code php pour les différentes instructions php que tu veux mettre en place pour l'inscription */
 
+<p> Bonjour </p>
+
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['prenom']; ?> !</p>
+
+<p>Si tu veux changer de prénom, <a href="Vues/Inscription.vue.php">clique ici</a> pour revenir à la page formulaire.php.</p>
 
 include_once('Vues/Inscription.vue.php');
 ?>
--- Vues/Inscription.vue.php
+++ Vues/Inscription.vue.php
@@ -9,6 +9,7 @@
 		<!-- ENTETE -->
         <header><?php include_once('includes/header.php'); ?></header>
         <!--corp-->
+        <form method="post" action="Inscription.php">
         <article class="art">
             <div class="boxtotale">
                     <div class="titrePage">
@@ -16,115 +17,117 @@
                     </div>
 
                     <div class="entreesInformations">
-                        <div class="conteneurChamp">
-                            
-                            <div class="partieAfficher">
-                                <label class="input-label" for="nom">
-                                    <p class="champTexte">Nom* :  
-                                    </p>
-                                    <input class="entreeDeTexte" type="charset" name="nom"/>
-                                </label>
-                            </div>
+                        
+                            <div class="conteneurChamp">
+                                
+                                <div class="partieAfficher">
+                                    <label class="input-label" for="nom">
+                                        <p class="champTexte">Nom* :  
+                                        </p>
+                                        <input class="entreeDeTexte" type="charset" name="nom"/>
+                                    </label>
+                                </div>
 
-                            <div class="partieCachee">
-                            </div>
-                        </div>
-                            
-
-                        <div class="conteneurChamp">    
-                            <div class="partieAfficher">
-                                <label class="input-label" for="prenom"><p class="champTexte">Prenom* :  
-                            </p>
-                                <input class="entreeDeTexte" type="charset" name="prenom" />
-                                </label>      
-                            </div>
                                 <div class="partieCachee">
                                 </div>
-                        </div>
+                            </div>
+                                
 
-                        
-
-                        <div class="conteneurChamp">
-                            <label class="input-label" for="mail">
-                                <p class="champTexte">Adresse E-mail* : 
+                            <div class="conteneurChamp">    
+                                <div class="partieAfficher">
+                                    <label class="input-label" for="prenom"><p class="champTexte">Prenom* :  
                                 </p>
-                                <input class="entreeDeTexte" type="charset" name="mail"/>
-                            </label>
-                            
-                        </div>
-
-                        <div class="conteneurChamp">
-                            <label class="input-label" for="login">
-                                <p class="champTexte">Pseudo* : 
-                                </p>
-                                <input class="entreeDeTexte" type="charset" name="login"/>
-                            </label>
-                            
-                        </div>
-
-                        <div class="conteneurChamp">
-                            
-                            <label class="input-label">
-                                <p class="champTexte">Mot de passe* : 
-                                </p>
-                                <input class="entreeDeTexte" type="charset" name="motDePasse"/>
-                            </label>
-                        </div>
-                        <div class="conteneurChamp">
-                            <label class="input-label"><p class="champTexte">Confirmez le mot de passe* : 
-                                </p>
-                                <input class="entreeDeTexte" type="charset" name="confirmMDP"/>
-                            </label>
-                        </div>
-                            
-                        
-                        <div class="conteneurChamp">
-                            <label class="input-label"><p class="champTexte">Type d'utilisateur* : 
-                                </p>
-                                <FORM>
-                                <SELECT class="liste" name="listeDeroulante" size="1">
-                                <OPTION value=0>Utilisateur</OPTION>
-                                <OPTION value=1>Utilisateur</OPTION>
-                                <OPTION value=2>Gestionnaire</OPTION>
-                                <OPTION value=3>Administrateur</OPTION>
-                                </SELECT>
-                            </FORM>
-                            </label>
-                            
-                        </div>
-                        <div class="conteneurChamp">
-                            <div class="champSepare">
-                                <label class="input-label">
-                                    <div class="textePlusInfoBulle">
-                                        <p class="champTexte">Code d'inscription* :    
-                                        </p>
-                                        <div class="infobulle"> 
-                                            <img src="ressources/aide.png" alt=" ? " style=width:13px;/> 
-                                            <span>Code fourni par votre établissement d'examen</span> 
-                                        </div>
+                                    <input class="entreeDeTexte" type="charset" name="prenom" />
+                                    </label>      
+                                </div>
+                                    <div class="partieCachee">
                                     </div>
-                                    
-                                    <input class="entreeDeTexte" type="charset" name="codeInscription"/>
+                            </div>
+
+                            
+
+                            <div class="conteneurChamp">
+                                <label class="input-label" for="mail">
+                                    <p class="champTexte">Adresse E-mail* : 
+                                    </p>
+                                    <input class="entreeDeTexte" type="charset" name="mail"/>
+                                </label>
+                                
+                            </div>
+
+                            <div class="conteneurChamp">
+                                <label class="input-label" for="login">
+                                    <p class="champTexte">Pseudo* : 
+                                    </p>
+                                    <input class="entreeDeTexte" type="charset" name="login"/>
+                                </label>
+                                
+                            </div>
+
+                            <div class="conteneurChamp">
+                                
+                                <label class="input-label">
+                                    <p class="champTexte">Mot de passe* : 
+                                    </p>
+                                    <input class="entreeDeTexte" type="charset" name="motDePasse"/>
                                 </label>
                             </div>
-                        </div>
-
+                            <div class="conteneurChamp">
+                                <label class="input-label"><p class="champTexte">Confirmez le mot de passe* : 
+                                    </p>
+                                    <input class="entreeDeTexte" type="charset" name="confirmMDP"/>
+                                </label>
+                            </div>
+                                
                             
-                    
+                            <div class="conteneurChamp">
+                                <label class="input-label"><p class="champTexte">Type d'utilisateur* : 
+                                    </p>
+                                    <FORM>
+                                    <SELECT class="liste" name="listeDeroulante" size="1">
+                                    <OPTION value=0></OPTION>
+                                    <OPTION value=1>Utilisateur</OPTION>
+                                    <OPTION value=2>Gestionnaire</OPTION>
+                                    <OPTION value=3>Administrateur</OPTION>
+                                    </SELECT>
+                                </FORM>
+                                </label>
+                                
+                            </div>
+                            <div class="conteneurChamp">
+                                <div class="champSepare">
+                                    <label class="input-label">
+                                        <div class="textePlusInfoBulle">
+                                            <p class="champTexte">Code d'inscription* :    
+                                            </p>
+                                            <div class="infobulle"> 
+                                                <img src="ressources/aide.png" alt=" ? " style=width:13px;/> 
+                                                <span>Code fourni par votre établissement d'examen</span> 
+                                            </div>
+                                        </div>
+                                        
+                                        <input class="entreeDeTexte" type="charset" name="codeInscription"/>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="conteneurChampCGU">
+                                <input type="checkbox" name="CGUaccepted" value="1">
+                                    <label>J'accepte les <a href="CGUMentionLegales.html">termes et conditions d'utilisation</a>
+                                    </label>
 
-                        <div class="conteneurChampCGU">
-                            <input type="checkbox" name="CGUaccepted" value="1"><label>J'accepte les <a href="CGUMentionLegales.html">termes et conditions d'utilisation</a></label>
-                        </div>
+                            </div>
 
-                        <div class="conteneurChamp">
-                            <input class="boutonValider" type="button" value="S'inscrire">
-                        </div>
+                            <div class="conteneurChamp">
+                                <input class="boutonValider" type="button" value="S'inscrire">
+                            </div>
+                        
                     </div>
                     <div class="asterix">
                         <p>* Champs obligatoire</p>
                     </div>
 
                 </div>
+                </form>
         </article>
         <?php
             include ("Vues/footer.php");
