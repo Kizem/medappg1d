32ecf0ca2e48aa03490747b22ee44927c8fc9611
Mohammad <mohammad.amla.pro@gmail.com>
2019-11-29 10:09:31 +0100

    Mohammad
    mise à jour du footer et ajout footer.vue

--- Inscription.php
+++ Inscription.php
@@ -1,10 +1,10 @@
 
 <?php
 if(isset($_POST['nom'])){
-	echo 'Votre nom est '.$_POST['nom'];
+	/*echo 'Votre nom est '.$_POST['nom'];
 }
 else{
-	echo 'nom pas declare';
+	echo 'nom pas declare';*/
 }
 include_once('Vues/Inscription.vue.php');
 ?>
