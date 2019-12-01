7639d35f4b441cb44c5d7720023ba9a5b48af970
Mohammad <mohammad.amla.pro@gmail.com>
2019-12-01 01:13:48 +0100

    inscritpion
    insertion base de donnee

--- Inscription.php
+++ Inscription.php
@@ -3,20 +3,24 @@
 
 
 require("Modeles/fonction.php");
-
-$pseudo = htmlspecialchars($_POST['login']);
-$nom = htmlspecialchars($_POST['nom']);
-$prenom = htmlspecialchars($_POST['prenom']);
-$mail = htmlspecialchars($_POST['mail']);
-$mdp1 = htmlspecialchars($_POST['motDePasse']);
-$mdp2 = htmlspecialchars($_POST['confirmMDP']);
-$typeUtilisateur = htmlspecialchars($_POST['listeDeroulante']);
-$codeInscription = htmlspecialchars($_POST['codeInscription']);
+require("includes/AccesBase.php");
 
 if(!empty($_POST['login']) AND !empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['mail']) AND !empty($_POST['motDePasse']) AND !empty($_POST['confirmMDP']) AND !empty($_POST['codeInscription']) AND ($_POST['listeDeroulante']!=0)){
 
+
+	$pseudo = htmlspecialchars($_POST['login']);
+	$nom = htmlspecialchars($_POST['nom']);
+	$prenom = htmlspecialchars($_POST['prenom']);
+	$mail = htmlspecialchars($_POST['mail']);
+	$mdp1 = htmlspecialchars($_POST['motDePasse']);
+	$mdp2 = htmlspecialchars($_POST['confirmMDP']);
+	$typeUtilisateur = htmlspecialchars($_POST['listeDeroulante']);
+	$codeInscription = htmlspecialchars($_POST['codeInscription']);
+
 	if(!empty($_POST['CGUaccepted'])){
+		echo "ca va";
 		if($mdp1==$mdp2){
+
 			switch ($typeUtilisateur) {
 				case '1':
 					$typeUtilisateur = "Utilisateur";
@@ -31,13 +35,16 @@ if(!empty($_POST['login']) AND !empty($_POST['nom']) AND !empty($_POST['prenom']
 					$typeUtilisateur = "Utilisateur";
 					break;
 			}
-			$db = new PDO("mysql:host=localhost;dbname="app", "root", ""); 
+			echo $_POST['confirmMDP'];
 			$req = insertUsers($db, $nom, $prenom, $mail, $mdp1, $typeUtilisateur, $pseudo);
-			echo $req;
+			echo '$nom';
+
+
+
 		}
 	}
 	else{
-		$erreur="Veuillez accepter les CGU et mentions légales pour vous inscrire";
+		echo "Veuillez accepter les CGU et mentions légales pour vous inscrire";
 	}
 
 }
