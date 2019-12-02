ad9059f8036209af40ed54a6ff8f33e72b1d2aa5
gragbar18 <57951672+gragbar18@users.noreply.github.com>
2019-12-02 16:18:15 +0100

    mise en forme des page connexion et inscription

--- Vues/connexion.vue.php
+++ Vues/connexion.vue.php
@@ -7,56 +7,68 @@
 	</head>
 	<body>
 		<!-- L'en-tête -->
-		<header><?php include_once('includes/header.php'); ?></header>	
+
+		<header>
+            <?php include_once('includes/header.php'); ?>
+        </header>	
+
 		<!-- Le contenu (1 seule section suffit) -->		
 		<section>
 			<form method="post" action="connexion.php ?>">
-			<h1 class="titre_formulaire">Connexion</h1>
+                <article class="art">
+                    <div class="boxtotale_2">
 
-			                        <div class="entreesInformations">
-                            
-                                <div class="conteneurChamp">
-                                    
-                                    <div class="partieAfficher">
-                                        <label class="input-label" for="nom">
-                                            <p class="champTexte">login* :  
-                                            </p>
-                                            <input class="entreeDeTexte" type="charset" name="login"required autofocus/>
-                                        </label>
-                                    </div>
+            			<h1 class="titre_formulaire">Connexion</h1>
 
-                                    <div class="partieCachee">
-                                    </div>
-                                </div>
-                                    
+            			                        <div class="entreesInformations">
+                                        
+                                            <div class="conteneurChamp">
+                                                
+                                                <div class="partieAfficher">
+                                                    <label class="input-label" for="nom">
+                                                        <p class="champTexte">login* :  
+                                                        </p>
+                                                        <input class="entreeDeTexte" type="charset" name="login"required autofocus/>
+                                                    </label>
+                                                </div>
 
-
-                                <div class="conteneurChamp">
-                                    
-                                    <label class="input-label">
-                                        <p class="champTexte">Mot de passe* : 
-                                        </p>
-                                        <input class="entreeDeTexte" type="password" name="Mdp"required autofocus/>
-                                    </label>
-                                </div>
-                      
+                                                <div class="partieCachee">
+                                                </div>
+                                            </div>
+                                                
 
 
-                                <div class="conteneurChamp">
-                                    <input class="boutonValider" type="submit" value="Connexion">				
-                                </div>
-                                <div class="Mdp_oublié">
-                                <a href= changepassword.php>mot de passe oublié?</a>
-                            	</div>
+                                            <div class="conteneurChamp">
+                                                
+                                                <label class="input-label">
+                                                    <p class="champTexte">Mot de passe* : 
+                                                    </p>
+                                                    <input class="entreeDeTexte" type="password" name="Mdp"required autofocus/>
+                                                </label>
+                                            </div>
+                                  
 
-				</p>
-				<?php 
-					include ("includes/AccesBase.php");
-				?>
-					
+
+                                            <div class="conteneurChamp">
+                                                <input class="boutonValider" type="submit" value="Connexion">				
+                                            </div>
+                                            <div class="Mdp_oublié">
+                                                <a href= changepassword.php>mot de passe oublié?</a>
+                                        	</div>
+
+            				
+        				<?php 
+        					include ("includes/AccesBase.php");
+        				?>
+        			</div>
+                </article>		
 			</form>
 		</section>
 
-		<footer><?php include_once('includes/footer.php'); ?></footer>
+		<footer>
+            <div class="footer_connexion">
+            <?php include_once('includes/footer.php'); ?>
+            </div> 
+        </footer>
 	</body>
 </html>
