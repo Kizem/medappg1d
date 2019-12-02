65551f21ca678665d0cf494b8c0f8bcb9b4a4f3e
geromono <58219411+geromono@users.noreply.github.com>
2019-12-02 12:00:40 +0100

    Première mise en forme des résultats

--- resultat_recherche.php
+++ resultat_recherche.php
@@ -3,10 +3,15 @@
 require("includes/AccesBase.php");
 require("Modeles/fonction.php");
 
+
+
 if(isset($_POST['keywords']) && !empty($_POST['keywords'])) {
 	$rep = rechercheUtilisateur($db, $_POST['keywords']);
 	$fonction = $rep->fetch();
+	//echo $fonction['Prenom'], " ", $fonction['Nom'];
+	include('Vues/resultat_recherche.vue.php');
 	echo $fonction['Prenom'], " ", $fonction['Nom'];
+
 
 }
 
