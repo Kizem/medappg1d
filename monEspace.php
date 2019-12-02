1e461d6a4db90e8280a9599c23902a987612002b
Samy_Zaouadi <samy.zaouadi@gmail.com>
2019-12-02 12:02:48 +0100

    modif monEspace

--- monEspace.php
+++ monEspace.php
@@ -1,8 +1,13 @@
 <?php
 
-if(!isset($_SESSION['admin'])){
+if(isset($_SESSION['admin']) OR $_SESSION['admin'] = 1){
 	include('page_admin.php');
 }
+else if(isset($_SESSION['user'])){
+
+	include('Utilisateur.php');
+}
+else(include('connexion.php'));
 
 
 ?>
