aab73d366f5b1091bb0623c8aa0b0d82ac0f9047
gragbar18 <57951672+gragbar18@users.noreply.github.com>
2019-12-02 10:17:58 +0100

    bouton top
    petit test de javascript

--- Vues/session_test.vue.php
+++ Vues/session_test.vue.php
@@ -3,6 +3,7 @@
   <head>
         <meta charset="utf-8" />
 	<link rel="stylesheet" type="text/css" href="design/session_test.css">
+  <link rel="stylesheet" type="javascript" href="Vues/test.js">
   <title>MedApp Project</title>
 
   </head>
@@ -11,6 +12,38 @@
     <div id = "main">
 
         <!--BODY --------------------------------- -->
+
+        <div>
+
+          <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+
+          <script>
+          //Get the button
+          var mybutton = document.getElementById("myBtn");
+
+          // When the user scrolls down 20px from the top of the document, show the button
+          window.onscroll = function() {scrollFunction()};
+
+          function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+              mybutton.style.display = "block";
+            } else {
+              mybutton.style.display = "none";
+            }
+          }
+
+          // When the user clicks on the button, scroll to the top of the document
+          function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+          }
+          </script>
+
+        </div>
+
+
+
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
         <div class="ban" align="center"> 
           <div class="test1" align="center">
             <img class="image" src="ressources/cardiac_test.jpg" alt="Alt text"/>
@@ -24,7 +57,10 @@
 
           <div class="test2" align="center">
             <img class="image" src="ressources/temperature_test.jpg" alt="Alt text"/>
-            <p class="table_header">TEST SUR LA TEMPERATURE CORPORELLE</p>
+            <p class="table_header">TEST SUR LA TEMPERATURE CORPORELLE 
+              <p class="table">Ce test de temparature va nous permettre d'obtenir plusieurs valeurs,
+              qui combiné a votre frequence cardiaque va nous donner une valeur precise de votre niveau de stress</p>
+            </p>
             <a href="nos_tests.php"></a><button class="do_tests">realiser le test</button>
           </div>
         </div>
