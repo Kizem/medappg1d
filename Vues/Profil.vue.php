81a86cd51684b95d8b0f794c8b2cc6f3ad795d11
Mohammad <mohammad.amla.pro@gmail.com>
2019-12-03 14:31:28 +0100

    profil
    ajout des poages profil

--- Vues/Profil.vue.php
+++ Vues/Profil.vue.php
@@ -9,6 +9,10 @@
         <!-- ENTETE -->
         <header><?php include_once('includes/header.php'); ?></header>
         <!--corp-->
+        <section class="main">
+        	
+        </section>
+        <!-- FOOTER -->
         <footer><?php include_once('includes/footer.php'); ?></footer>
         
     </body>
