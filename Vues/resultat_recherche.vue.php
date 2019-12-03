99fd90c9385015dbf00c39ab2330522d80a4ef8f
geromono <58219411+geromono@users.noreply.github.com>
2019-12-03 15:32:47 +0100

    MàJ du moteur de recherche et de la FAQ

--- Vues/resultat_recherche.vue.php
+++ Vues/resultat_recherche.vue.php
@@ -6,6 +6,41 @@
 		<header><?php include_once('includes/header.php'); ?></header>
 	<body>
 
+		<h2>Résultat de la recherche :<br/></h2>
+
+		<div>
+			<table style="width:80%">
+				<!--
+				<tr>
+    				<th>Nom</th>
+    				<th>Prénom</th>
+  				</tr>
+  				-->
+
+  				<?php 
+  				if (count($fonction) > 0) {
+  					for ($i=0; $i < count($fonction); $i++) {
+  						echo $fonction[$i]['Nom'], ' ';
+  						echo $fonction[$i]['Prenom'], ' ',"<br>";
+  					}
+  				}
+  				else{
+  					echo "Pas d'utilisateurs";
+  				}
+  				
+
+  				?>
+  				<!--
+  				<tr>
+  					<th> <?php echo $fonction[0]['Nom']; ?> </th>
+  					<th> <?php echo $fonction[0]['Prenom']; ?> </th>
+  				</tr>
+  				-->
+  				
+			</table>
+		</div>
+
+
 
 		<footer><?php include_once('includes/footer.php'); ?></footer>
 	</body>
--- database/app.sql
+++ database/app.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 25 nov. 2019 à 08:41
--- Version du serveur :  5.7.21
--- Version de PHP :  5.6.35
+-- Généré le :  mar. 03 déc. 2019 à 14:05
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `app`
 --
-CREATE DATABASE IF NOT EXISTS `app` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `app`;
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +48,42 @@ CREATE TABLE IF NOT EXISTS `capteur` (
   `seuil` double NOT NULL,
   PRIMARY KEY (`idCapteur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cgu`
+--
+
+DROP TABLE IF EXISTS `cgu`;
+CREATE TABLE IF NOT EXISTS `cgu` (
+  `id` int(25) NOT NULL AUTO_INCREMENT,
+  `MentionLegales` mediumtext NOT NULL,
+  `CGU` mediumtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `codeinscription`
+--
+
+DROP TABLE IF EXISTS `codeinscription`;
+CREATE TABLE IF NOT EXISTS `codeinscription` (
+  `id` varchar(25) NOT NULL,
+  `fonction` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `codeinscription`
+--
+
+INSERT INTO `codeinscription` (`id`, `fonction`) VALUES
+('azerty', 'Utilisateur'),
+('qsdfgh', 'Gestionnaire'),
+('wxcvbn', 'Administrateur');
 
 -- --------------------------------------------------------
 
@@ -110,42 +145,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `codeInscription`
---
-
-DROP TABLE IF EXISTS `codeInscription`;
-CREATE TABLE IF NOT EXISTS `codeInscription` (
-  `id` varchar(25) NOT NULL ,
-  `fonction` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `codeInscription` 
---
-
-INSERT INTO codeInscription (id,fonction) VALUES
-('azerty','Utilisateur'),
-('qsdfgh','Gestionnaire'),
-('wxcvbn','Administrateur');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `CGU`
---
-
-DROP TABLE IF EXISTS `CGU`;
-CREATE TABLE IF NOT EXISTS `CGU`(
-  `id` INT(25) NOT NULL AUTO_INCREMENT,
-  `MentionLegales` TEXT(100000) NOT NULL,
-  `CGU` TEXT(100000) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `utilisateur`
 --
 
@@ -159,17 +158,18 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Prenom` varchar(100) CHARACTER SET latin1 NOT NULL,
   `Mail` varchar(100) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 --
--- Dumping data for table `utilisateur`
+-- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO utilisateur (Type, login, Mdp, Nom, Prenom, Mail) VALUES 
-('Administrateur', 'Moh2a91', 'tqtpas', 'AMLA', 'Mohammad','mohammad.amla.pro@gmail.com');
+INSERT INTO `utilisateur` (`idUser`, `Type`, `login`, `Mdp`, `Nom`, `Prenom`, `Mail`) VALUES
+(1, 'Administrateur', 'Moh2a91', 'tqtpas', 'AMLA', 'Mohammad', 'mohammad.amla.pro@gmail.com'),
+(3, 'Utilisateur', 'momo', '$2y$10$e6K0GruoBnIs9qfad3xmcOKo1o9nB0ZaP6.SyytQ6hFcSRwLaWkea', 'AMLE', 'Mohammed', 'jerotm@pmfpoj.com'),
+(4, 'Utilisateur', 'tyty', '$2y$10$hLk642ZBF0lSndviVJE4QOprCPWTeuwUcrd8ufC05YaCHAKQX8Sxa', 'LOPEZ', 'Ammad', 'youpu@lkjml.com');
 COMMIT;
--- --------------------------------------------------------
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
