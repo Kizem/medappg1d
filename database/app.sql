eb36aa21b54567b6bdac3a7e8a5971d79ab745b2
Mohammad <mohammad.amla.pro@gmail.com>
2019-12-03 22:43:57 +0100

    nouvelle base de donnee

--- database/app.sql
+++ database/app.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 03 déc. 2019 à 14:05
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Généré le :  mar. 03 déc. 2019 à 14:55
+-- Version du serveur :  5.7.21
+-- Version de PHP :  5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,7 +21,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `app`
 --
-
+/* Mohammad 03/12/19 : Il faut créer la base !!!!*/
+CREATE DATABASE IF NOT EXISTS `app` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `app`;
 -- --------------------------------------------------------
 
 --
@@ -31,7 +33,11 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `boitier`;
 CREATE TABLE IF NOT EXISTS `boitier` (
   `idBoitier` int(11) NOT NULL,
-  PRIMARY KEY (`idBoitier`)
+  `Date` date NOT NULL,
+  `idCapteur` int(11) NOT NULL,
+  PRIMARY KEY (`idBoitier`),
+  KEY `idCapteur` (`idCapteur`),
+  KEY `Date` (`Date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -48,42 +54,6 @@ CREATE TABLE IF NOT EXISTS `capteur` (
   `seuil` double NOT NULL,
   PRIMARY KEY (`idCapteur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `cgu`
---
-
-DROP TABLE IF EXISTS `cgu`;
-CREATE TABLE IF NOT EXISTS `cgu` (
-  `id` int(25) NOT NULL AUTO_INCREMENT,
-  `MentionLegales` mediumtext NOT NULL,
-  `CGU` mediumtext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `codeinscription`
---
-
-DROP TABLE IF EXISTS `codeinscription`;
-CREATE TABLE IF NOT EXISTS `codeinscription` (
-  `id` varchar(25) NOT NULL,
-  `fonction` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `codeinscription`
---
-
-INSERT INTO `codeinscription` (`id`, `fonction`) VALUES
-('azerty', 'Utilisateur'),
-('qsdfgh', 'Gestionnaire'),
-('wxcvbn', 'Administrateur');
 
 -- --------------------------------------------------------
 
@@ -111,8 +81,25 @@ CREATE TABLE IF NOT EXISTS `message` (
   `Date` date NOT NULL,
   `Heure` datetime NOT NULL,
   `contenu` text CHARACTER SET latin1 NOT NULL,
+  `idUser` int(255) NOT NULL,
   PRIMARY KEY (`idMessage`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messageuser`
+--
+
+DROP TABLE IF EXISTS `messageuser`;
+CREATE TABLE IF NOT EXISTS `messageuser` (
+  `idMessageUser` int(11) NOT NULL AUTO_INCREMENT,
+  `idMessage` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  PRIMARY KEY (`idMessageUser`),
+  KEY `idMessage` (`idMessage`),
+  KEY `idUser` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
 
@@ -125,7 +112,9 @@ CREATE TABLE IF NOT EXISTS `test` (
   `Date` date NOT NULL,
   `Type` varchar(100) CHARACTER SET latin1 NOT NULL,
   `Durée` datetime NOT NULL,
-  PRIMARY KEY (`Date`)
+  `idUser` int(255) NOT NULL,
+  PRIMARY KEY (`Date`),
+  KEY `idUser` (`idUser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- --------------------------------------------------------
@@ -143,6 +132,51 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+/* Mohammad 03/12/19 : table pour les codes d'inscriptions. Surement à modifier*/
+--
+-- Structure de la table `codeInscription`
+--
+
+DROP TABLE IF EXISTS `codeInscription`;
+CREATE TABLE IF NOT EXISTS `codeInscription` (
+  `id` varchar(25) NOT NULL ,
+  `fonction` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `codeInscription` 
+--
+
+INSERT INTO codeInscription (id,fonction) VALUES
+('azerty','Utilisateur'),
+('qsdfgh','Gestionnaire'),
+('wxcvbn','Administrateur');
+
+-- --------------------------------------------------------
+/* Mohammad 03/12/19 : Table pour les cgu et ml*/
+--
+-- Structure de la table `CGU`
+--
+
+DROP TABLE IF EXISTS `CGU`;
+CREATE TABLE IF NOT EXISTS `CGU`(
+  `idCGU` INT(25) NOT NULL AUTO_INCREMENT,
+  `MentionLegales` TEXT(100000) NOT NULL,
+  `CGU` TEXT(100000) NOT NULL,
+  PRIMARY KEY (`idCGU`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/* Mohammad 03/12/19 : dumping data pour cgu*/
+--
+-- Dumping data for table `utilisateur`
+--
+
+INSERT INTO CGU (idCGU, MentionLegales, CGU) VALUES 
+('1', 'Vos Mentions legales...', 'Vos CGU...');
+
+
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `utilisateur`
@@ -151,23 +185,67 @@ CREATE TABLE IF NOT EXISTS `users` (
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
-  `Type` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `Type` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
   `login` varchar(100) CHARACTER SET latin1 NOT NULL,
   `Mdp` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `Nom` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `Prenom` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `Mail` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `Nom` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `Prenom` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `Mail` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `codeInscription` varchar(255) COLLATE latin1_general_cs DEFAULT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+/* Mohammad 03/12/19 : dumping data pour utilisateur*/
+--
+-- Dumping data for table `utilisateur`
+--
+
+INSERT INTO utilisateur (Type, login, Mdp, Nom, Prenom, Mail) VALUES 
+('Administrateur', 'Moh2a91', 'tqtpas', 'AMLA', 'Mohammad','mohammad.amla.pro@gmail.com');
+COMMIT;
+-- --------------------------------------------------------
 
 --
--- Déchargement des données de la table `utilisateur`
+-- Structure de la table `utilisateur/entité`
 --
 
-INSERT INTO `utilisateur` (`idUser`, `Type`, `login`, `Mdp`, `Nom`, `Prenom`, `Mail`) VALUES
-(1, 'Administrateur', 'Moh2a91', 'tqtpas', 'AMLA', 'Mohammad', 'mohammad.amla.pro@gmail.com'),
-(3, 'Utilisateur', 'momo', '$2y$10$e6K0GruoBnIs9qfad3xmcOKo1o9nB0ZaP6.SyytQ6hFcSRwLaWkea', 'AMLE', 'Mohammed', 'jerotm@pmfpoj.com'),
-(4, 'Utilisateur', 'tyty', '$2y$10$hLk642ZBF0lSndviVJE4QOprCPWTeuwUcrd8ufC05YaCHAKQX8Sxa', 'LOPEZ', 'Ammad', 'youpu@lkjml.com');
+DROP TABLE IF EXISTS `utilisateur/entité`;
+CREATE TABLE IF NOT EXISTS `utilisateur/entité` (
+  `idU/E` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `idEntité` int(11) NOT NULL,
+  PRIMARY KEY (`idU/E`),
+  KEY `idEntité` (`idEntité`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `boitier`
+--
+ALTER TABLE `boitier`
+  ADD CONSTRAINT `boitier_ibfk_1` FOREIGN KEY (`idCapteur`) REFERENCES `capteur` (`idCapteur`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `boitier_ibfk_2` FOREIGN KEY (`Date`) REFERENCES `test` (`Date`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `messageuser`
+--
+ALTER TABLE `messageuser`
+  ADD CONSTRAINT `messageuser_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `utilisateur` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `test`
+--
+ALTER TABLE `test`
+  ADD CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `utilisateur` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `utilisateur/entité`
+--
+ALTER TABLE `utilisateur/entité`
+  ADD CONSTRAINT `utilisateur/entité_ibfk_1` FOREIGN KEY (`idEntité`) REFERENCES `entit` (`idEntité`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
