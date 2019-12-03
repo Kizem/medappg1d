c04d18d8fe1589cc652d0f6f9ef6a1d290ec2a3b
Mohammad <mohammad.amla.pro@gmail.com>
2019-12-03 14:48:51 +0100

    tqt

--- Vues/Profil.vue.php
+++ Vues/Profil.vue.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="design/Inscription.css"> 
+    <link rel="stylesheet" type="text/css" href="design/Profil.css"> 
         <title>Page de connexion</title>
     </head>
     <body>
@@ -10,7 +10,9 @@
         <header><?php include_once('includes/header.php'); ?></header>
         <!--corp-->
         <section class="main">
-        	
+        	<aside>
+        		
+        	</aside>
         </section>
         <!-- FOOTER -->
         <footer><?php include_once('includes/footer.php'); ?></footer>
