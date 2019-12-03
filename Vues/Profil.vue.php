ce1a005f3565c0f9ba3ae000d7eec5d8b52e26d4
Mohammad <mohammad.amla.pro@gmail.com>
2019-12-03 14:18:24 +0100

    page profil

---
+++ Vues/Profil.vue.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="design/Inscription.css"> 
+        <title>Page de connexion</title>
+    </head>
+    <body>
+        <!-- ENTETE -->
+        <header><?php include_once('includes/header.php'); ?></header>
+        <!--corp-->
+        <footer><?php include_once('includes/footer.php'); ?></footer>
+        
+    </body>
+</html>
