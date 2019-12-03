9175e52e2f0012fde972425c4d7f80aa8576c75f
Edinhodsr <edinhodsr@gmail.com>
2019-12-03 15:55:34 +0100

    billy

--- Vues/Utilisateur.vue.php
+++ Vues/Utilisateur.vue.php
@@ -13,37 +13,42 @@
         
         <table>
 
-        <thead> <!-- En-tête du tableau -->
-       <tr>
-           <th> </th>
-           <th>Valeur minimale</th>
-           <th>Valeur moyenne</th>
-           <th>Valeur maximale</th>
-       </tr>
-        </thead>
+            <thead> <!-- En-tête du tableau -->
+        <tr>
+            <th> </th>
+            <th>Dernière valeur</th>
+            <th>Valeur minimale</th>
+            <th>Valeur moyenne</th>
+            <th>Valeur maximale</th>
+        </tr>
+            </thead>
 
-        <tbody>
-            <tr>
-                <td>Fréquence cardiaque</td>
-                <td>Valeurs</td>
-                <td>Valeurs</td>
-                <td>Valeurs</td>
-            </tr>
-            <tr>
-                <td>Température du corps</td>
-                <td>Valeurs</td>
-                <td>Valeurs</td>
-                <td>Valeurs</td>
-            </tr>
-            <tr>
-                <td>Perception auditive</td>
-                <td>Valeurs</td>
-                <td>Valeurs</td>
-                <td>Valeurs</td>
-            </tr>
-        </tbody>    
+            <tbody>
+                <tr>
+                    <td>Fréquence cardiaque</td>
+                    <td><?php echo $donnees;?></td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                </tr>
+                <tr>
+                    <td>Température du corps</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                </tr>
+                <tr>
+                    <td>Perception auditive</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                    <td>Valeurs</td>
+                </tr>
+            </tbody>    
         </table>    
        
+        <!-- FOOTER -->
         <footer><?php include_once('includes/footer.php');?>
 	</body>
 </html>
