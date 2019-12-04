e20359407f86a3cac93337a11786a48b5ac5306e
Mohammad <mohammad.amla.pro@gmail.com>
2019-12-04 21:23:21 +0100

    msaj page profil

--- Vues/Profil.vue.php
+++ Vues/Profil.vue.php
@@ -10,9 +10,59 @@
         <header><?php include_once('includes/header.php'); ?></header>
         <!--corp-->
         <section class="main">
-        	<aside class="">
-        		
-        	</aside>
+            <table class="tableInformations">
+                <tbody>
+                	<tr>
+                        <td colspan="2" class=photo>
+                            <br>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="texteAuCentre">
+                            <p>Prenom</p>
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="texteAuCentre">
+                            <p>Nom</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="Separation">
+                            <hr>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="texteAuCentre">
+                            <p>Informations de base</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            Pseudo
+                        </th>
+                        <td>
+                            Mettre le pseudo
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="Separation">
+                            <hr>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="texteAuCentre">
+                            <p>Informations avancées</p>
+                        </td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+            <div class="ConteneurCentral">
+                
+            </div>
         </section>
         <!-- FOOTER -->
         <footer><?php include_once('includes/footer.php'); ?></footer>
