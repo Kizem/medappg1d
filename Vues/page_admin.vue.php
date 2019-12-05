804287991c4669b138b30cf211fc1162aff0999d
Samy_Zaouadi <samy.zaouadi@gmail.com>
2019-12-05 02:00:04 +0100

    page admin : fonction supprimer

--- Vues/page_admin.vue.php
+++ Vues/page_admin.vue.php
@@ -16,18 +16,34 @@
 
       <div class="options">
         <div class="catégorie">
+          <ul>
+            <?php while($u = $utilisateurs->fetch()){ ?>
+              <li><?= $u['idUser'] ?> : <?=$u['login'] ?>
+              - <?= $u['Mail'] ?></li>
+              <li class="liAction"><a href="page_admin.php?delete=<?= $u['idUser']?>">Supprimer</a></li>
 
-          <p>test</p>
+            <?php } ?>
+          </ul>
+        <!--   <ul class="actions">
+
+            <li class="liAction"><?php<a href="index.php">Ajouter</a>?></li>
+            <li class="liAction"><a href="page_admin.php?delete=1>">Supprimer</a></li>
+            <li class="liAction"><a href="index.php">Modifier</a></li>
+            <li class="liAction"><a href="index.php">Bannir</a></li>
+
+
+
+          </ul> -->
 
         </div>
 
         <div class="catégorie">
-          <p>test</p>
+          <p>Gestion des capteurs</p>
 
         </div>
 
         <div class="catégorie">
-          <p>test</p>
+          <p>FAQ/CGU + Messagerie interne</p>
           
 
         </div>
--- page_admin.php
+++ page_admin.php
@@ -5,5 +5,26 @@ inclure la page administrateur
 
 */
 include_once("includes/AccesBase.php");
+include_once("Modeles/fonction.php");
+
+//maj_session($db,$_SESSION['idUser']);
+
+//Si pas de connexion, ou pas administrateur
+if(!isset($_SESSION) OR $_SESSION['Type'] != 'Administrateur'){
+	exit();
+}
+//on vient tout d'abord effectuer une mise à jour des infos de la session avant de modifier
+
+
+if(isset($_GET['delete']) AND !empty($_GET['delete'])){
+	//on vient supprimer l'utilisateur qui a pour id la valeur de ? dans l'url
+	$req = $db->prepare('DELETE FROM utilisateur WHERE idUser = ?');
+	$req -> execute(array($_GET['delete']));
+
+
+}
+
+$utilisateurs = $db->query('SELECT * FROM utilisateur ORDER BY idUser DESC'); //permet de classer les users par ordre d'arrivée (les derniers en tête)
+
 include_once('Vues/page_admin.vue.php');
 ?>
