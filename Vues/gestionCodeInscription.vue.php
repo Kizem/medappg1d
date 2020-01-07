036de50d98eabbddc4e01c597d28e73b46e6b69d
Mohammad <mohammad.amla.pro@gmail.com>
2020-01-07 22:10:25 +0100

    ajout page

---
+++ Vues/gestionCodeInscription.vue.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+        <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="design/Utilisateur.css"> 
+		<title>Page Utilisateur</title>
+	</head>
+    
+	<body>
+		<!-- ENTETE -->
+        <header><?php include_once('includes/header.php'); ?></header>
+        <!--corp-->
+        
+        
+       
+
+	</body>
+</html>
---
+++ gestionCodeInscription.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once('includes/AccesBase.php');
+include_once('Modeles/fonction.php');
+
+
+
+include_once('Vues/gestionCodeInscription.vue.php');
+?>
