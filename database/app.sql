9d4495de9a1edfacc3098db4aa466a6ea9b659c1
Mohammad <mohammad.amla.pro@gmail.com>
2020-01-07 23:05:12 +0100

    maj bdd

--- database/app.sql
+++ database/app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 07 jan. 2020 à 15:15
+-- Généré le :  mar. 07 jan. 2020 à 21:58
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -45,22 +45,6 @@ INSERT INTO `boitier` (`idBoitier`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `boitier_capteur`
---
-
-DROP TABLE IF EXISTS `boitier_capteur`;
-CREATE TABLE IF NOT EXISTS `boitier_capteur` (
-  `id_boitier_test` int(11) NOT NULL AUTO_INCREMENT,
-  `idCapteur` int(11) NOT NULL,
-  `idBoitier` int(11) NOT NULL,
-  PRIMARY KEY (`id_boitier_test`),
-  KEY `idCapteur` (`idCapteur`),
-  KEY `idBoitier` (`idBoitier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `capteur`
 --
 
@@ -70,16 +54,18 @@ CREATE TABLE IF NOT EXISTS `capteur` (
   `Type` varchar(100) CHARACTER SET latin1 NOT NULL,
   `Valeur` double NOT NULL,
   `seuil` double NOT NULL,
-  `Date` Date DEFAULT NULL,
-  PRIMARY KEY (`idCapteur`)
+  `Date` date DEFAULT NULL,
+  `idBoitier` int(11) NOT NULL,
+  PRIMARY KEY (`idCapteur`),
+  KEY `idBoitier` (`idBoitier`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 --
 -- Déchargement des données de la table `capteur`
 --
 
-INSERT INTO `capteur` (`idCapteur`, `Type`, `Valeur`, `seuil`) VALUES
-(1, 'Pouls', 1, 85);
+INSERT INTO `capteur` (`idCapteur`, `Type`, `Valeur`, `seuil`, `Date`, `idBoitier`) VALUES
+(1, 'Pouls', 1, 85, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -100,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `cgu` (
 --
 
 INSERT INTO `cgu` (`idCGU`, `MentionLegales`, `CGU`) VALUES
-(1, 'bonjour mounir le fdp mange ma banane', 'et mais toi tes voila la salope');
+(1, 'Vos cgu....', 'Vos mentions lelaes...');
 
 -- --------------------------------------------------------
 
@@ -134,6 +120,8 @@ DROP TABLE IF EXISTS `entit`;
 CREATE TABLE IF NOT EXISTS `entit` (
   `idEntité` int(11) NOT NULL AUTO_INCREMENT,
   `Type` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `Nom` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `Adresse` varchar(100) CHARACTER SET latin1 NOT NULL,
   `Gestionnaire` varchar(100) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`idEntité`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
@@ -236,7 +224,8 @@ CREATE TABLE IF NOT EXISTS `utilisateur/entité` (
   `idUser` int(11) NOT NULL,
   `idEntité` int(11) NOT NULL,
   PRIMARY KEY (`idU/E`),
-  KEY `idEntité` (`idEntité`)
+  KEY `idEntité` (`idEntité`),
+  KEY `idUser` (`idUser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
@@ -244,11 +233,10 @@ CREATE TABLE IF NOT EXISTS `utilisateur/entité` (
 --
 
 --
--- Contraintes pour la table `boitier_capteur`
+-- Contraintes pour la table `capteur`
 --
-ALTER TABLE `boitier_capteur`
-  ADD CONSTRAINT `boitier_test_ibfk_1` FOREIGN KEY (`idBoitier`) REFERENCES `boitier` (`idBoitier`) ON DELETE CASCADE,
-  ADD CONSTRAINT `boitier_test_ibfk_2` FOREIGN KEY (`idCapteur`) REFERENCES `capteur` (`idCapteur`) ON DELETE CASCADE;
+ALTER TABLE `capteur`
+  ADD CONSTRAINT `capteur_ibfk_1` FOREIGN KEY (`idBoitier`) REFERENCES `boitier` (`idBoitier`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `messageuser`
@@ -273,7 +261,8 @@ ALTER TABLE `utilisateur`
 -- Contraintes pour la table `utilisateur/entité`
 --
 ALTER TABLE `utilisateur/entité`
-  ADD CONSTRAINT `utilisateur/entité_ibfk_1` FOREIGN KEY (`idEntité`) REFERENCES `entit` (`idEntité`) ON DELETE CASCADE;
+  ADD CONSTRAINT `utilisateur/entité_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `utilisateur` (`idUser`) ON DELETE CASCADE,
+  ADD CONSTRAINT `utilisateur/entité_ibfk_2` FOREIGN KEY (`idEntité`) REFERENCES `entit` (`idEntité`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
