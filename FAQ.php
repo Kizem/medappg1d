19b79b66da58c5c5cdd4d9db7e089544612886c5
gragbar18 <57951672+gragbar18@users.noreply.github.com>
2020-01-08 21:26:17 +0100

    creation de l'envoi de mail
    il faut fake sendmail pour que cela fonctionne avec wamp

--- FAQ.php
+++ FAQ.php
@@ -6,4 +6,22 @@ Page d'acceuil du site. Affichage des 4 derniers cours mis en ligne.
 */
 
 include_once('Vues/FAQ.vue.php');
+
+$to = "MedAppG1D@gmail.com";
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$from = $_POST['from'];
+
+$headers = "From: $from";
+
+//$from = $_POST['from'];
+
+//$message = $_POST['message'];
+
+//$body = "this is an automated message. Please do not reply to this email. \n\n $message";
+//$headers = "From: $from";
+
+mail($to, $subject, $message, $headers);
 ?>
