d000cf8da362916e7379b2aba8fae41a1bd4958e
geromono <58219411+geromono@users.noreply.github.com>
2020-01-08 17:53:20 +0100

    Update edit faq

--- Edition_FAQ.php
+++ Edition_FAQ.php
@@ -22,15 +22,7 @@ elseif (isset($_POST['Theme3']) && !empty($_POST['Theme3'])) {
 
 else{
 
-	echo '<form action="index.php" method="get">
-			Question :  <textarea name="ML" rows="1" cols="70" required></textarea></br>
-			Réponse :  <textarea name="ML" rows="10" cols="70" required></textarea></br>
-			<input type="hidden" name="id" value="<?php echo $id; ?>"/>
-			<input type="hidden" name="action" value="save"/>
-			<input type="submit"/>
-		</form>';
-
-	
+	$requete = 1;
 
 	include_once('Vues/Edition_FAQ.vue.php');
 }
--- Vues/Edition_FAQ.vue.php
+++ Vues/Edition_FAQ.vue.php
@@ -9,20 +9,76 @@
     <body>
         <header><?php include_once('includes/header.php'); ?></header>
 
+        <?php $requete = 0; ?>
+
         <form name="Edition de la FAQ" method="post" action=".../Edition_FAQ.php">
 
             <h2 id="Nom_Theme1">Thème n°1</h2>
             Veuillez indiquer le nom du thème n°1 : <input type="text" name="Theme1" placeholder="bile" /> <br/>
 
-            <input type="submit" value="Ajouter une question" class="bouton_ajouter">
+
+            <?php 
+
+            if($requete==0){
+               echo '<input type="submit" value="Ajouter une question" class="bouton_ajouter">';
+            }
+
+            elseif (condition) {
+                echo '<form action="index.php" method="get">
+                    Question :  <textarea name="ML" rows="1" cols="70" required></textarea></br>
+                    Réponse :  <textarea name="ML" rows="10" cols="70" required></textarea></br>
+                    <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                    <input type="hidden" name="action" value="save"/>
+                    <input type="submit"/>
+                  </form>';
+                $requete=0;
+            
+            } ?>
+
 
 
             <h2 id="Nom_Theme2">Thème n°2</h2>
             Veuillez indiquer le nom du thème n°2 : <input type="text" name="Theme2"/> <br/>
 
+            <?php 
+
+            if($requete==0){
+               echo '<input type="submit" value="Ajouter une question" class="bouton_ajouter">';
+            }
+
+            elseif (condition) {
+                echo '<form action="index.php" method="get">
+                    Question :  <textarea name="ML" rows="1" cols="70" required></textarea></br>
+                    Réponse :  <textarea name="ML" rows="10" cols="70" required></textarea></br>
+                    <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                    <input type="hidden" name="action" value="save"/>
+                    <input type="submit"/>
+                  </form>';
+                $requete=0;
+            
+            } ?>
+
 
             <h2 id="Nom_Theme3">Thème n°3</h2>
             Veuillez indiquer le nom du thème n°3 : <input type="text" name="Theme3"/> <br/>
+
+            <?php 
+
+            if($requete==0){
+               echo '<input type="submit" value="Ajouter une question" class="bouton_ajouter">';
+            }
+
+            elseif (condition) {
+                echo '<form action="index.php" method="get">
+                    Question :  <textarea name="ML" rows="1" cols="70" required></textarea></br>
+                    Réponse :  <textarea name="ML" rows="10" cols="70" required></textarea></br>
+                    <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                    <input type="hidden" name="action" value="save"/>
+                    <input type="submit"/>
+                  </form>';
+                $requete=0;
+            
+            } ?>
 
         </form>
 
