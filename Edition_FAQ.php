2def9c257e91968ffa8f6ab2053d676225fa637e
geromono <58219411+geromono@users.noreply.github.com>
2020-01-08 17:33:09 +0100

    Update Edition_FAQ.php

--- Edition_FAQ.php
+++ Edition_FAQ.php
@@ -5,21 +5,18 @@ require("includes/AccesBase.php");
 if(isset($_POST['Theme1']) && !empty($_POST['Theme1'])){
 	$id = 1;
 	$rep = changerNomTheme($db, $_POST['Theme1'], $id);
-
 	include_once('Vues/Edition_FAQ.vue.php');
 }
 
 elseif (isset($_POST['Theme2']) && !empty($_POST['Theme2'])) {
 	$id = 2;
 	$rep = changerNomTheme($db, $_POST['Theme2'], $id);
-	
 	include_once('Vues/Edition_FAQ.vue.php');
 }
 
 elseif (isset($_POST['Theme3']) && !empty($_POST['Theme3'])) {
 	$id = 3;
 	$rep = changerNomTheme($db, $_POST['Theme3'], $id);
-	
 	include_once('Vues/Edition_FAQ.vue.php');
 }
 
